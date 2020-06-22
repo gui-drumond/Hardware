0b701b442fdebacde24cf9503c6913ed86ccf99c
Skillado <drumond.guilherme@hotmail.com>
2020-06-22 15:10:39 -0300

    ok

--- _js/script.js
+++
@@ -1,7 +0,0 @@
-var i = setInterval(function () {
-    clearInterval(i);
-
-    // este codigo mostra o delay do termino de mostrar uma div:
-    //usei este codigo com um fake loading in css para sumir apos alguns segundos.
-    document.getElementById("content").style.display = "none";
-}, 2500);
